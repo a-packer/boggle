e55cd6e80bf33c71ef1a1cecdd32f18ebda63030
Aubrey <aubreypacker5@gmail.com>
2020-12-18 13:44:18 -0700

    First commit to boggle game

---
+++ static/boggle.js
@@ -0,0 +1,46 @@
+class BoggleGame {
+  /* make a new game at this DOM id */
+
+  constructor(boardId) {
+
+    this.score = 0;
+    this.words = new Set();
+    this.board = $("#" + boardId);
+    $(".submit-word").on("submit", this.handleSubmit.bind(this));
+  }
+
+  /* show word in list of words */
+  showWord(word) {
+    $(".words", this.board).append($("<li>", { text: word }));
+  }
+
+  /* show score in html */
+  showScore() {
+    // select score return the highscore
+  }
+
+  /* show a status message */
+  showMessage(msg, cls) {
+    $("message-text") // return msg
+  }
+
+  /* handle submission of word: if unique and valid, score & show message`*/
+  async handleSubmit(evt) {
+    evt.preventDefault();
+    const $word = $(".word", this.board);
+
+    let word = $word.val();
+    if (!word) return;
+
+    // check server for validity
+    const resp = await axios.get("/check_word", { params: { word: word }});
+    if (resp.data.result === "not-word") {
+      this.showMessage(`${word} is not a valid English word`, "err");
+    } else if (resp.data.result === "not-on-board") {
+      this.showMessage(`${word} is not a valid word on this board`, "err");
+    } else {
+      this.showWord(word);
+    }
+  }
+
+}
